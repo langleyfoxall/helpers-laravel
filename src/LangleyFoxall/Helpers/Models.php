15595bedc111d38a8b30e2cafd31ae24798595e4
Jordan Hall <jordan@hall05.co.uk>
2018-07-25 16:06:23 +0100

    Rework method to load all models regarding of whether that have been loaded

--- src/LangleyFoxall/Helpers/Models.php
+++ src/LangleyFoxall/Helpers/Models.php
@@ -4,12 +4,54 @@ namespace LangleyFoxall\Helpers;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Models
+ * @package LangleyFoxall\Helpers
+ */
 abstract class Models
 {
+    /**
+     * Returns a collections of class names for all Eloquent models within your app path.
+     *
+     * @return \Illuminate\Support\Collection
+     */
     public static function all()
     {
-        return collect(get_declared_classes())->filter(function($class) {
-            return is_subclass_of($class, Model::class);
+        $command = 'grep --include="*.php" --files-with-matches -r "class" '.app_path();
+
+        exec($command, $files);
+
+        return collect($files)->map(function($file) {
+            return self::convertFileToClass($file);
+        })->filter(function($class) {
+            return class_exists($class) && is_subclass_of($class, Model::class);
         });
+    }
+
+    /**
+     * Converts a file name to a namespaced class name.
+     *
+     * @param $file
+     * @return string
+     */
+    private static function convertFileToClass($file)
+    {
+        $fh = fopen($file, 'r');
+
+        $namespace = '';
+
+        while(($line = fgets($fh, 5000)) !== false) {
+
+            if (str_contains($line, 'namespace')) {
+                $namespace = trim(str_replace(['namespace', ';'], '', $line));
+                break;
+            }
+        }
+
+        fclose($fh);
+
+        $class = basename(str_replace('.php', '', $file));
+
+        return $namespace.'\\'.$class;
     }
 }
