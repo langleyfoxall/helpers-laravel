83054defb4cc0fa1c4100d1085d5cdc27e6d594a
Jordan Hall <jordan@hall05.co.uk>
2018-08-31 15:16:03 +0100

    IsRelatedTo trait

---
+++ src/LangleyFoxall/Helpers/Traits/IsRelatedTo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LangleyFoxall\Helpers\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use LangleyFoxall\Helpers\Models;
+
+trait IsRelatedTo
+{
+    /**
+     * Check if the current model is related to a specified model
+     *
+     * @param Model|array $relations
+     * @throws \InvalidArgumentException|\Exception
+     * @return bool
+     */
+    function isRelatedTo($model)
+    {
+        return Models::areRelated($this, $model);
+    }
+
+}
