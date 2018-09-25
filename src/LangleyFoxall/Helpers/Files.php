e711e787a581ddf26a7550fe3477ca9b59963cdf
Dexter Marks-Barber <dexter.mb@outlook.com>
2018-09-25 08:59:24 +0100

    Add initial Files helper - Add method filter method

---
+++ src/LangleyFoxall/Helpers/Files.php
@@ -0,0 +1,31 @@
+<?php
+namespace LangleyFoxall\Helpers;
+
+class Files
+{
+    /**
+	 * Return a filtered array of files in a given directory
+	 * 
+	 * @param string|array $pattern
+	 * @param string $dir
+	 * @return array
+	 */
+    public static function filter($pattern, string $dir)
+	{
+		if (!ends_with($dir, '/*')) {
+			$dir = $dir . '/*';
+		}
+
+		$patterns = is_array($pattern) ? $pattern : [$pattern];
+
+        return collect(glob($dir))->filter(function ($file) use ($patterns) {
+            foreach($patterns as $pattern) {
+                if (!!preg_match($pattern, $file)) {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+	}
+}
